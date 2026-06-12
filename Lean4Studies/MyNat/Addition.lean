4928c865566f7f7fff9d31836393ca35f784e0c3
Anthony Reynolds <92904564+areyn123@users.noreply.github.com>
2026-06-13 07:44:35 +1000

    Prove addition theorems: zero_add, succ_add, add_comm, add_assoc, add_right_comm

--- Lean4Studies/MyNat/Addition.lean
+++ Lean4Studies/MyNat/Addition.lean
@@ -12,13 +12,39 @@ def add : MyNat → MyNat → MyNat
 
 instance : Add MyNat where add := add
 
-theorem add_zero (a : MyNat) : a + 0 = a := rfl
+theorem add_zero (a : MyNat) : a + zero = a := rfl
 theorem add_succ (a b : MyNat) : a + succ b = succ (a + b) := rfl
 
-theorem zero_add (n : MyNat) : 0 + n = n := sorry
-theorem succ_add (a b : MyNat) : succ a + b = succ (a + b) := sorry
-theorem add_comm (a b : MyNat) : a + b = b + a := sorry
-theorem add_assoc (a b c : MyNat) : a + b + c = a + (b + c) := sorry
-theorem add_right_comm (a b c : MyNat) : a + b + c = a + c + b := sorry
+theorem zero_add (n : MyNat) : zero + n = n := by
+  induction n with
+    | zero => rfl
+    | succ k ih => rw [add_succ, ih]
+
+theorem succ_add (a b : MyNat) : succ a + b = succ (a + b) := by
+  induction b with
+    | zero => rfl
+    | succ k ih => rw [add_succ, ih, add_succ]
+
+theorem add_comm (a b : MyNat) : a + b = b + a := by
+  induction b with
+    | zero => rw [add_zero, zero_add]
+    | succ k ih => rw [add_succ, ih, succ_add]
+
+theorem add_assoc (a b c : MyNat) : a + b + c = a + (b + c) := by
+  induction c with
+    | zero => rw [add_zero, add_zero, add_comm]
+    | succ k ih =>
+      conv => lhs; rw [add_succ]
+      conv => rhs; rw [add_succ]
+      rw  [ih, add_succ]
+
+theorem add_right_comm (a b c : MyNat) : a + b + c = a + c + b := by
+  induction c with
+    | zero => rw [add_zero, add_zero]
+    | succ k ih =>
+    conv => lhs; rw [add_succ, ih]
+    conv => rhs; rw [add_assoc, succ_add, add_succ, ← add_assoc]
+
+
 
 end MyNat
